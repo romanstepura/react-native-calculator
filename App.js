41d4fa545da3ec857bb7a3d690763be6b6d18f6c
vasya <vasja.kish@mail.ru>
2019-12-04 14:48:40 +0200

    Add styles

--- App.js
+++ App.js
@@ -7,31 +7,82 @@
  */
 
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+// eslint-disable-next-line no-unused-vars
+import {StyleSheet, View, Text,Button} from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import Keyboard from './Components/Keyboard';
+
 const App: () => React$Node = () => {
   return (
-    <View>
-      <Keyboard />
+    <View style={styles.container}>
+      <View style={styles.result} />
+      <View style={styles.calculation} />
+      <View style={styles.buttons}>
+        <View style={styles.nutmegs}>
+          <View style={styles.row}>
+            <Button title="0" />
+            <Button title="0" />
+            <Button title="0" />
+          </View>
+          <View style={styles.row}>
+            <Button title="0" />
+            <Button title="0" />
+            <Button title="0" />
+          </View>
+          <View style={styles.row}>
+            <Button title="0" />
+            <Button title="0" />
+            <Button title="0" />
+          </View>
+          <View style={styles.row}>
+            <Button title="0" />
+            <Button title="0" />
+            <Button title="0" />
+          </View>
+        </View>
+        <View style={styles.operations}>
+          <Button title="+"></Button>
+          <Button title="+"></Button>
+          <Button title="+"></Button>
+          <Button title="+"></Button>
+
+        </View>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create();
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row:{
+    flexDirection: 'row',
+    flex:1,
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  result: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  calculation: {
+    flex: 1,
+    backgroundColor: 'green',
+  },
+  buttons: {
+    flexGrow: 5,
+    flexDirection: 'row',
+  },
+  nutmegs: {
+    flex: 3,
+    backgroundColor: 'yellow',
+  },
+  operations: {
+    flex: 1,
+    justifyContent: 'space-around',
+    backgroundColor: 'black',
+  },
+});
 
 export default App;
